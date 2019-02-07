a2e911ff9cdd56eed55422d2422060ea22494736
hugofsiqueira <hugo.fsiqueira@gmail.com>
2019-02-06 21:48:26 -0300

    alteracao

---
+++ src/com/qualiti/banco/dados/mapa/ClienteDAOMapImpl.java
@@ -0,0 +1,37 @@
+package com.qualiti.banco.dados.mapa;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import com.qualiti.banco.dados.ClienteDAO;
+import com.qualiti.banco.modelo.Cliente;
+
+public class ClienteDAOMapImpl implements ClienteDAO {
+	
+	private Map<String, Cliente> repositorio;
+	
+	public ClienteDAOMapImpl() {
+		repositorio = new Hashtable<>();
+	}
+
+	@Override
+	public void inserir(Cliente cliente) {
+		repositorio.put(cliente.getCpf(), cliente);
+	}
+
+	@Override
+	public void atualizar(Cliente cliente) {
+		repositorio.put(cliente.getCpf(), cliente);
+	}
+
+	@Override
+	public void remover(String cpf) {
+		repositorio.remove(cpf);
+	}
+
+	@Override
+	public Cliente procurar(String cpf) {
+		return repositorio.get(cpf);
+	}
+
+}
